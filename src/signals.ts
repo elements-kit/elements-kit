1f00a8f4d41559e04bc67fed8185d940471f7849
Wael Bettayeb <bettayebwael@gmail.com>
2026-03-28 17:51:58 +0400

    refactor: reorganize imports and add batch and untracked functions in signals

--- src/signals.ts
+++ src/signals.ts
@@ -1,5 +1,12 @@
 export * from "alien-signals";
-import { isSignal, isComputed, signal } from "alien-signals";
+import {
+  isSignal,
+  isComputed,
+  signal,
+  setActiveSub,
+  startBatch,
+  endBatch,
+} from "alien-signals";
 import type { ValueOrReactive } from "./core";
 
 export function isReactive<T>(value: ValueOrReactive<T>): value is () => T {
@@ -7,3 +14,29 @@ export function isReactive<T>(value: ValueOrReactive<T>): value is () => T {
 }
 
 export type Signal<T> = ReturnType<typeof signal<T>>;
+
+/**
+ * @param {function(): void} fn
+ */
+export const batch = (fn) => {
+  startBatch();
+  try {
+    fn();
+  } finally {
+    endBatch();
+  }
+};
+
+/**
+ * @template T
+ * @param {function(): T} fn
+ * @returns {T}
+ */
+export const untracked = (fn) => {
+  const sub = setActiveSub(void 0);
+  try {
+    return fn();
+  } finally {
+    setActiveSub(sub);
+  }
+};
