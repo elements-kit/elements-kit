de8c7195cb22dd2aa432811a805efca434ab6527
Wael Bettayeb <bettayebwael@gmail.com>
2026-03-31 11:15:43 +0400

    fix: update return type of get method in Slot class to include null for better type safety

--- src/slot.ts
+++ src/slot.ts
@@ -54,7 +54,7 @@ export class Slot {
     parent.insertBefore(element, this.end);
   }
 
-  get(): DocumentFragment {
+  get(): DocumentFragment | null {
     if (!this.isMounted()) return null;
     const range = document.createRange();
     range.setStartAfter(this.start);
